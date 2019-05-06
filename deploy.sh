7146915efcfae6d800b5f9c05dd60006fe3b6b9e
jenkins <jenkins@icgc.com>
2019-05-06 13:07:04 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 
-
 #!/bin/bash
 
 rm -fr /home/tic/mapa-delinquencial
 cp -r tmp /home/tic/mapa-delinquencial
 
-
-rm -fr /var/www/html/mapa-delinquencial
+mkdir -p /var/www/html/mapa-delinquencial
+rm -fr /var/www/html/mapa-delinquencial/*
 cp -r /home/tic/mapa-delinquencial/*  /var/www/html/mapa-delinquencial/
 
 
