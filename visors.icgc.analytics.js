caee1eda766f88a965654f44fb9dc5c75009f200
VictorPA <sfggeogis@gmail.com>
2019-05-08 13:04:48 +0200

    fix error

--- visors.icgc.analytics.js
+++ visors.icgc.analytics.js
@@ -1,26 +1,7 @@
 
-/*
-var link = document.createElement("link");
-link.id = "CookiesICGCCSS";
-link.setAttribute("rel", "stylesheet");
-link.setAttribute("type", "text/css");
-link.setAttribute("href", "https://gencat.github.io/ICGC-Cookie-GDPR/dist/cookies-icgc.min.css");
-link.addEventListener("load", function() {
-
-	var cookies = document.createElement("script");
-	cookies.id = "CookiesICGCScript";
-	cookies.setAttribute("src", "https://gencat.github.io/ICGC-Cookie-GDPR/dist/cookies-icgc.js")
-	document.head.appendChild(cookies);
-	cookies.addEventListener("load", function() {
-
-		const cuqui = new icgc.CookiesICGC("visors.icgc.cat", ["UA-46332195-2"], {
-			content: {
-				config: "GDPR"
-			}
-		});
-
-*/
-
+/**
+ * 
+ */
 (function(i, s, o, g, r, a, m) {
 	i['GoogleAnalyticsObject'] = r;
 	i[r] = i[r] || function() {
@@ -38,24 +19,18 @@ ga('send', 'pageview');
 
 
 
-		if (typeof console !== 'undefined' && typeof console.log === 'function' && !window.test) {
+if (typeof console !== 'undefined' && typeof console.log === 'function' && !window.test) {
 
-		console.debug("Fet per @ICGCGeoStart - Institut Cartogràfic i Geològic de Catalunya ICGC.\n"+
-		"******************** \n"+
-		"Sergio Anguita Rovira \n"+
-		"Isaac Besora Vilardaga \n"+	
-		"Montserrat Ortega Gallart \n"+
-		"Victor Pascual Ayats \n"+
-		"Gabriel Reynal Blanch \n"+
-		"Rafael Roset Arisso \n"+
-		"Jessica Sena Garcia \n"+
-		"Wladimir Szczerban Llatas \n"+
-		"Marc Torres Saura \n"+
-		"********************");
-	
-		}
+console.debug("Fet per GeoStart - Institut Cartogràfic i Geològic de Catalunya ICGC.\n"+
+"******************** \n"+
+"Sergio Anguita Rovira \n"+
+"Isaac Besora Vilardaga \n"+	
+"Montserrat Ortega Gallart \n"+
+"Victor Pascual Ayats \n"+
+"Rafael Roset Arisso \n"+
+"Jessica Sena Garcia \n"+
+"Wladimir Szczerban Llatas \n"+
+"Marc Torres Saura \n"+
+"********************");
 
-	});
-
-});
-document.head.appendChild(link);
+}
