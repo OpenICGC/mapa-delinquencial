8bb3f230b6da56bcd2d6acabff8545d813a07d0a
VictorPA <sfggeogis@gmail.com>
2019-05-08 11:49:59 +0200

    add index

--- visors.icgc.analytics.js
+++ visors.icgc.analytics.js
@@ -1,3 +1,5 @@
+
+/*
 var link = document.createElement("link");
 link.id = "CookiesICGCCSS";
 link.setAttribute("rel", "stylesheet");
@@ -16,6 +18,23 @@ link.addEventListener("load", function() {
 				config: "GDPR"
 			}
 		});
+
+*/
+
+(function(i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script',
+		'//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-46332195-2', 'auto');
+ga('send', 'pageview');
 
 
 
